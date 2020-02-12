d4a0bd6fae9d6e51aa9a2a2b41052ec0c99b3a42
Diego <diego@diegosilva.com.br>
2020-02-12 09:25:40 -0300

    correcoes

---
+++ src/main/java/br/com/diegosilva/bank/routes/BankAccountRoutes.java
@@ -0,0 +1,108 @@
+package br.com.diegosilva.bank.routes;
+
+import akka.actor.typed.ActorSystem;
+import akka.actor.typed.javadsl.Adapter;
+import akka.cluster.sharding.typed.javadsl.ClusterSharding;
+import akka.cluster.sharding.typed.javadsl.EntityRef;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.model.StatusCodes;
+import akka.http.javadsl.server.Route;
+import akka.serialization.jackson.JacksonObjectMapperProvider;
+import br.com.diegosilva.bank.actors.account.BankAccount;
+import br.com.diegosilva.bank.states.BankAccountState;
+import br.com.diegosilva.bank.utils.UUIDGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.util.Date;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.CompletionStage;
+
+import static akka.http.javadsl.server.Directives.*;
+
+public class BankAccountRoutes {
+
+    public static class CreateAccount {
+        public final String uid;
+        public final String name;
+        public final BigDecimal startAmmount;
+
+        public CreateAccount(String uid, String name, BigDecimal startAmmount) {
+            this.uid = uid;
+            this.name = name;
+            this.startAmmount = startAmmount;
+        }
+    }
+
+
+    private final ActorSystem<?> system;
+    private final ClusterSharding sharding;
+    private final Duration timeout;
+    private final ObjectMapper objectMapper;
+
+    public BankAccountRoutes(ActorSystem<?> system) {
+        this.system = system;
+        sharding = ClusterSharding.get(system);
+        timeout = system.settings().config().getDuration("shopping.askTimeout");
+        // Use Jackson ObjectMapper from Akka Jackson serialization
+        objectMapper = JacksonObjectMapperProvider.get(Adapter.toClassic(system))
+                .getOrCreate("jackson-json", Optional.empty());
+    }
+
+
+    public Route shopping() {
+        return pathPrefix("bank", () ->
+                pathPrefix("account", () ->
+                        concat(
+                                post(() ->
+                                        entity(
+                                                Jackson.unmarshaller(objectMapper, CreateAccount.class),
+                                                data -> onConfirmationReply(createAccount(data))))
+                        )
+                )
+        );
+    }
+
+    private Route onConfirmationReply(CompletionStage<BankAccount.Confirmation> reply) {
+        return onSuccess(reply, confirmation -> {
+            if (confirmation instanceof BankAccount.Accepted)
+                return complete(StatusCodes.OK, ((BankAccount.Accepted) confirmation).summary, Jackson.marshaller(objectMapper));
+            else
+                return complete(StatusCodes.BAD_REQUEST, ((BankAccount.Rejected) confirmation).reason);
+        });
+    }
+
+    private CompletionStage<BankAccount.Confirmation> createAccount(CreateAccount data) {
+        String accountNumber = UUID.randomUUID().toString();
+        EntityRef<BankAccount.Command> entityRef =
+                sharding.entityRefFor(BankAccount.ENTITY_TYPE_KEY, accountNumber);
+        return entityRef.ask(replyTo -> new BankAccount.CreateAccount(accountNumber, data.name, data.uid,
+                new BankAccountState.Transaction(UUIDGenerator.generateType4UUID().toString(), new Date().getTime(),
+                        data.startAmmount, data.uid, data.uid, "D"), replyTo), timeout);
+    }
+//
+//  private CompletionStage<ShoppingCart.Confirmation> updateItem(UpdateItem data) {
+//    EntityRef<ShoppingCart.Command> entityRef =
+//      sharding.entityRefFor(ShoppingCart.ENTITY_TYPE_KEY, data.cartId);
+//    if (data.quantity == 0)
+//      return entityRef.ask(replyTo -> new ShoppingCart.RemoveItem(data.itemId, replyTo), timeout);
+//    else
+//      return entityRef.ask(replyTo -> new ShoppingCart.AdjustItemQuantity(data.itemId, data.quantity, replyTo), timeout);
+//  }
+//
+//  private CompletionStage<ShoppingCart.Summary> getCart(String cartId) {
+//    EntityRef<ShoppingCart.Command> entityRef =
+//      sharding.entityRefFor(ShoppingCart.ENTITY_TYPE_KEY, cartId);
+//    return entityRef.ask(ShoppingCart.Get::new, timeout);
+//  }
+//
+//  private CompletionStage<ShoppingCart.Confirmation> checkout(String cartId) {
+//    EntityRef<ShoppingCart.Command> entityRef =
+//      sharding.entityRefFor(ShoppingCart.ENTITY_TYPE_KEY, cartId);
+//    return entityRef.ask(ShoppingCart.Checkout::new, timeout);
+//  }
+
+
+}
