c9c34eae0f7941a2e973ba8c9740ad858423fda1
Diego <diego@diegosilva.com.br>
2020-02-12 14:55:48 -0300

    correcoes

--- src/main/java/br/com/diegosilva/bank/routes/BankAccountRoutes.java
+++ src/main/java/br/com/diegosilva/bank/routes/BankAccountRoutes.java
@@ -45,7 +45,7 @@ public class BankAccountRoutes {
     public BankAccountRoutes(ActorSystem<?> system) {
         this.system = system;
         sharding = ClusterSharding.get(system);
-        timeout = system.settings().config().getDuration("shopping.askTimeout");
+        timeout = system.settings().config().getDuration("bank.askTimeout");
         // Use Jackson ObjectMapper from Akka Jackson serialization
         objectMapper = JacksonObjectMapperProvider.get(Adapter.toClassic(system))
                 .getOrCreate("jackson-json", Optional.empty());
@@ -82,27 +82,4 @@ public class BankAccountRoutes {
                 new BankAccountState.Transaction(UUIDGenerator.generateType4UUID().toString(), new Date().getTime(),
                         data.startAmmount, data.uid, data.uid, "D"), replyTo), timeout);
     }
-//
-//  private CompletionStage<ShoppingCart.Confirmation> updateItem(UpdateItem data) {
-//    EntityRef<ShoppingCart.Command> entityRef =
-//      sharding.entityRefFor(ShoppingCart.ENTITY_TYPE_KEY, data.cartId);
-//    if (data.quantity == 0)
-//      return entityRef.ask(replyTo -> new ShoppingCart.RemoveItem(data.itemId, replyTo), timeout);
-//    else
-//      return entityRef.ask(replyTo -> new ShoppingCart.AdjustItemQuantity(data.itemId, data.quantity, replyTo), timeout);
-//  }
-//
-//  private CompletionStage<ShoppingCart.Summary> getCart(String cartId) {
-//    EntityRef<ShoppingCart.Command> entityRef =
-//      sharding.entityRefFor(ShoppingCart.ENTITY_TYPE_KEY, cartId);
-//    return entityRef.ask(ShoppingCart.Get::new, timeout);
-//  }
-//
-//  private CompletionStage<ShoppingCart.Confirmation> checkout(String cartId) {
-//    EntityRef<ShoppingCart.Command> entityRef =
-//      sharding.entityRefFor(ShoppingCart.ENTITY_TYPE_KEY, cartId);
-//    return entityRef.ask(ShoppingCart.Checkout::new, timeout);
-//  }
-
-
 }
